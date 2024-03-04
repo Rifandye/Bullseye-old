9b7cbe76ec4b88197d166536f72460a4626b4f31
Rifandye <rilfransyah@gmail.com>
2024-03-04 19:26:42 +0700

    style: layouting register page

--- bullseye/src/app/register/page.tsx
+++ bullseye/src/app/register/page.tsx
@@ -1,7 +1,58 @@
+import "./register.css";
+
 export default function RegisterPage() {
   return (
-    <main>
-      <h1>Ini untuk register</h1>
+    <main
+      className="flex min-h-screen w-full justify-center items-center"
+      style={{
+        backgroundImage: "url('/regisbackground.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="card">
+        <div className="card2">
+          <form className="form">
+            <p id="heading">Register</p>
+            <div className="field">
+              <input
+                type="text"
+                className="input-field"
+                placeholder="Name"
+                autoComplete="off"
+              />
+            </div>
+            <div className="field">
+              <input
+                type="text"
+                className="input-field"
+                placeholder="Username"
+                autoComplete="off"
+              />
+            </div>
+            <div className="field">
+              <input
+                type="email"
+                className="input-field"
+                placeholder="Email"
+                autoComplete="off"
+              />
+            </div>
+            <div className="field">
+              <input
+                type="password"
+                className="input-field"
+                placeholder="Password"
+              />
+            </div>
+            <div className="btn">
+              <button type="submit" className="button1">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </main>
   );
 }
